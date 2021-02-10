48d779a197654e8a566bfd114ec28ed5ea311612
Ropponngi <THU-Tokyo@outlook.com>
2021-02-10 17:32:11 +0800

    Create HistorySection.swift

---
+++ WeeklyHabitTracker/Utilities/HistorySection.swift
@@ -0,0 +1,14 @@
+//
+//  HistorySection.swift
+//  WeeklyHabitTracker
+//
+//  Created by Jason Wang on 6/13/20.
+//  Copyright © 2020 Jason Wang. All rights reserved.
+//
+
+import Foundation
+
+enum HistorySection {
+    case activeHabits
+    case finishedHabits
+}
