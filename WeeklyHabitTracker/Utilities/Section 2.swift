8d20c512c3dcd81718b9fcd408a31ff674a1930a
Ropponngi <THU-Tokyo@outlook.com>
2021-02-10 17:32:45 +0800

    Create Section 2.swift

---
+++ WeeklyHabitTracker/Utilities/Section 2.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  WeeklyHabitTracker
+//
+//  Created by Robert Parsons on 4/27/20.
+//  Copyright © 2020 Robert Parsons. All rights reserved.
+//
+
+import Foundation
+
+enum Section {
+    case main
+}
