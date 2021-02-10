77fd744792c06640b1b2efdde4f71c5bd2e15a13
Ropponngi <THU-Tokyo@outlook.com>
2021-02-10 17:32:07 +0800

    Create CVSection.swift

---
+++ WeeklyHabitTracker/Utilities/CVSection.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  WeeklyHabitTracker
+//
+//  Created by Jason Wang on 4/27/20.
+//  Copyright © 2020 Jason Wang. All rights reserved.
+//
+
+import Foundation
+
+enum CVSection {
+    case main
+}
