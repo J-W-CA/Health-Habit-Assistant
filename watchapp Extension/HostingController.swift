22f77451ef43e964fe531a2b0d91b0285d3f8035
Ropponngi <THU-Tokyo@outlook.com>
2021-02-17 16:02:17 +0800

    Create HostingController.swift

---
+++ watchapp Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Pomosh Watch Extension
+//
+//  Created by Steven J. Selcuk on 3.06.2020.
+//  Copyright © 2020 Steven J. Selcuk. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
