4c2cff1bc175ffd2861999f69bd878eb3d80a415
Ropponngi <THU-Tokyo@outlook.com>
2021-02-17 16:11:01 +0800

    Update SceneDelegate.swift

--- WeeklyHabitTracker/Support/SceneDelegate.swift
+++ WeeklyHabitTracker/Support/SceneDelegate.swift
@@ -15,10 +15,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        window?.windowScene = windowScene
-        window?.rootViewController = MainTabBarController()
-        window?.makeKeyAndVisible()
+//        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+//        window?.windowScene = windowScene
+//        window?.rootViewController = MainTabBarController()
+//        window?.makeKeyAndVisible()
+        let isExistingUser = DataStore.shared.getUserStatus()
+        let controller: UIViewController!
+
+        if isExistingUser {
+            controller = MainTabBarController()
+        } else {
+            DataStore.shared.setUserStatus(isExistingUser: true)
+            controller = UINavigationController(rootViewController: GetStartedVC())
+        }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = controller
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
