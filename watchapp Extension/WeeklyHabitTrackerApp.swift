b0e1b55cd31b50c3dba7f15a6ec478b913761142
Ropponngi <THU-Tokyo@outlook.com>
2021-02-17 16:11:06 +0800

    Create WeeklyHabitTrackerApp.swift

---
+++ watchapp Extension/WeeklyHabitTrackerApp.swift
@@ -0,0 +1,22 @@
+//
+//  WeeklyHabitTrackerApp.swift
+//  watchapp Extension
+//
+//  Created by Jason Wang on 13/2/2021.
+//  Copyright © 2021 Jason Wang. All rights reserved.
+//
+
+//import SwiftUI
+//
+//@main
+//struct WeeklyHabitTrackerApp: App {
+//    @SceneBuilder var body: some Scene {
+//        WindowGroup {
+//            NavigationView {
+//                ContentView()
+//            }
+//        }
+//
+//        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+//    }
+//}
