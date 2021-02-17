2e66e2c30c409995561d9726f9abb306014ecebd
Ropponngi <THU-Tokyo@outlook.com>
2021-02-17 16:11:11 +0800

    Create NotificationView.swift

---
+++ watchapp Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  watchapp Extension
+//
+//  Created by Jason Wang on 13/2/2021.
+//  Copyright © 2021 Jason Wang. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
